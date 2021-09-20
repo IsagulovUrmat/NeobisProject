abdc05dc41175085751f9687f7a63ffb16aa0c61
Isagulov urmat <isagulovurmat@MacBook-Pro-Isagulov.local>
2021-09-21 00:45:38 +0600

    final

--- NeobisProject/NeobisProject/urls.py
+++ NeobisProject/NeobisProject/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path("", include("movies.urls")),
 ]
+
+# urlpatterns += i18n_patterns(
+#     path('accounts/', include('allauth.urls')),
+# )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
